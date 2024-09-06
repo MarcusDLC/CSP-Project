283be45f55a0cd6a67b2a6bbcee6f59445dcc864
MarcusDLC <marcus.delacruz@ucas-edu.net>
2024-09-06 20:41:46 +0000

    new variables assigned and variables printed

--- test folder/test.py
+++ test folder/test.py
@@ -1,1 +1,13 @@
-print("Code spaces is awesome")
+
+name = "Marcus De La Cruz"
+print(name)
+school = "UCAS"
+print(school)
+number = "7"
+print(number)
+year = "2024"
+print (year)
+hundreds = "435"
+print (hundreds)
+eye = "brown"
+print(eye)
