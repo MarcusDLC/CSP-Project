1991ba928e6dafc1a110ab1197f77544a6b855bd
MarcusDLC <marcus.delacruz@ucas-edu.net>
2024-09-07 17:44:02 +0000

    Assigned variables and print statements made for variables.

--- test folder/test.c
+++ test folder/test.c
@@ -4,3 +4,23 @@ int main(void){
     printf("Hello World!");
     return 0;
 }
+name ="Marcus De La Cruz";
+printf(name);
+school ="UCAS";
+printf(school);
+number ="8";
+print(number);
+year ="2024";
+print(year);
+hundreds ="934";
+print(hundreds);
+eye ="Brown";
+print(eye);
+breakfast ="Eggs and toast";
+print(breakfast);
+age ="14";
+print(age);
+color ="Blue";
+print(blue);
+subject ="Mathematics"; 
+print(subject);
