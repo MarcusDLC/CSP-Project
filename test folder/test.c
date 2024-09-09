d4d923f65c576259f83342f2f3bd783e93bfc901
MarcusDLC <marcus.delacruz@ucas-edu.net>
2024-09-09 20:28:19 +0000

    Fixed arrangement of code and print statements

--- test folder/test.c
+++ test folder/test.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
 
-int main(void){
-    printf("Hello World!");
+int main(void) { 
+    char name []="Marcus De La Cruz \n";
+    printf(name);
+    char school []="UCAS\n"; 
+    printf(school);
+    char number []="8\n"; 
+    printf(number);
+    char year []="2024\n";
+    printf(year);
+    char hundreds []="934\n";
+    printf(hundreds);
+    char eye []="Brown\n";
+    printf(eye);
+    char breakfast[]="Eggs and toast\n";
+    printf(breakfast);
+    char age []="14\n";
+    printf(age);
+    char color[] ="Blue\n";
+    printf(color);
+    char subject []="Mathematics\n"; 
+    printf(subject);
     return 0;
 }
-name ="Marcus De La Cruz";
-printf(name);
-school ="UCAS";
-printf(school);
-number ="8";
-printf(number);
-year ="2024";
-printf(year);
-hundreds ="934";
-printf(hundreds);
-eye ="Brown";
-printf(eye);
-breakfast ="Eggs and toast";
-printf(breakfast);
-age ="14";
-printf(age);
-color ="Blue";
-printf(blue);
-subject ="Mathematics"; 
-printf(subject);
