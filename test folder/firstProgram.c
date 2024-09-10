0eaf301e8cfbbd014dfdae32a34bc14f50c8d57d
MarcusDLC <marcus.delacruz@ucas-edu.net>
2024-09-10 20:24:29 +0000

    Made C language give an output with a user input

--- test folder/firstProgram.c
+++ test folder/firstProgram.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 int main(void) {
-char user_input input="What is your name?:"
-print(,user_input)
+    char user_input [30];
+    printf("What is your name?:\n");
+    scanf("%s", user_input);
+    printf("Hello %s",user_input);
+    return 0;
 }
