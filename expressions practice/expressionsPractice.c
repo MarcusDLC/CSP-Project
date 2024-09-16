07c487e3b94638d5bdbdf18268319759c3fa26c9
MarcusDLC <marcus.delacruz@ucas-edu.net>
2024-09-16 20:34:22 +0000

    FInished expressions assignment in C

--- expressions practice/expressionsPractice.c
+++ expressions practice/expressionsPractice.c
@@ -2,15 +2,16 @@
 #include <math.h> 
 
 int main(void) {
+    int power=(int)pow(3,4);
     
     printf("%d\n", 7-24/8*4+6);
     printf("%d\n", 18/3-7+2*5);
     printf("%d\n", 6*4/12+72/8-9);
     printf("%d\n", (17-6/2)+4*3);
-    printf("%d\n", -2(1*4-2/2)+(6+2-3));
-    printf("%d\n", -1*[(3-4*7)/5]-2*24/6);
-    printf("%d\n", 3*5**/15)-(5-2**);
-    printf("%d\n", (1**** 2**+ 3***)-2***** /4);
-    printf("%d\n", );
+    printf("%d\n", -2*(1*4-2/2)+(6+2-3));
+    printf("%d\n", -1*((3-4*7)/5)-2*24/6);
+    printf("%d\n", 3*(int)pow(5,2)/15)-(5-(int)pow(2,2));
+    printf("%d\n", (int)pow(1,4)*(int)pow(2,2)+ (int)pow(3,3)-(int)pow(2,5) /4);
+    printf("%d\n", (int)pow((22/2-2*5), 2)+(int)pow((4-6/6), 2));
     return 0;
 }
