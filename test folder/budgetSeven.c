9f7db40ae32ad98e7756b667d366ce7bca1d6738
MarcusDLC <marcus.delacruz@ucas-edu.net>
2024-09-16 20:07:28 +0000

    Finished the financial calculator assignment

--- test folder/budgetSeven.c
+++ test folder/budgetSeven.c
@@ -2,7 +2,7 @@
 
 int main(void){
     float income, transportation, rent, utilities, groceries, expenses, savings, total;
-    float prent, putilities, ptransportation, pgroceries, pexpenses;
+    float prent, putilities, ptransportation, pgroceries, pexpenses, psavings;
     printf("This is going to calculate your budget for you\n");
     printf("How much do you make a month?\n");
     scanf("%f", &income);
@@ -31,5 +31,7 @@ int main(void){
     printf("Your transportation is %.2f", ptransportation, "of your income\n");
     printf("Your groceries  is %.2f", pgroceries, "of your income\n");
     printf("Your utitilies is %.2f", putilities, "of your income\n");
+    printf("Your savings is %.2f", psavings, "of your income.\n");
+    printf("Your expenses is %.2f", pexpenses, "of your income.\n");
     return 0;
 }
