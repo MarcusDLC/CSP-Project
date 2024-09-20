0301f1a06407dd3db788c8562d8bd4d300942271
MarcusDLC <marcus.delacruz@ucas-edu.net>
2024-09-20 20:41:50 +0000

    Started a madlib

---
+++ strings practice/sillyMadlib.py
@@ -0,0 +1,17 @@
+
+adjective = input("Type in an adjective!")
+adjective2 = input("Type in another adjective!")
+birdtype = input("Type in a type of bird!")
+houseroom = input("Type in a room of a house!")
+pastverb = input("Type in an past tense verb!")
+verb = input("Type in an verb!")
+relative = input("Type in another adjective!")
+noun = input("Type in an noun!")
+liquid = input("Type in a type of liquid!")
+ingverb = input("Type in a verb ending in ING!")
+bodypart = input("Type in a body part!")
+pluralnoun2 = input("Type in a plural noun!")
+ingverb2 = input("Type in another verb ending in ING!")
+noun2 = input("Type in another noun!")
+
+Madlib = "It was a " + adjective + " , cold day in November. I woke up to the " + adjective2 + " smell of " + birdtype
