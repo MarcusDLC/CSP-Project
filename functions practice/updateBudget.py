e25869813b3c47d32a296c51481c334cd93abed7
MarcusDLC <marcus.delacruz@ucas-edu.net>
2024-09-27 20:32:47 +0000

    Finished the updated budget assignment in Python

--- functions practice/updateBudget.py
+++ functions practice/updateBudget.py
@@ -1,12 +1,14 @@
 
 def dollars(type):
-    return("How much is your {type}? ")
 
-
+    return input(f"How much is your {type}:")
 
 print("This is going to calculate your budget for you")
 income = float(dollars("income"))
-
+rent = float(dollars("rent"))
+utilities = float(dollars("utilities"))
+groceries = float(dollars("groceries"))
+transportation = float(dollars("transportation"))
 
 expenses = rent + utilities + groceries + transportation
 savings = income * .2
@@ -19,25 +21,18 @@ pexpenses = (expenses / income)* 100
 psavings = (savings / income)* 100
 
 def percent(type, amount):
-    per = amount/income*100
+    percentage = amount/income*100
     
-    return (f"Your {type} is {amount}% of your income.")
+    return(f"Your {type} is {int (percentage)}% of your income.")
 
 print(f"Your income is: ${income:.2f}")
 print(f"Your expenses are: ${expenses:.2f}")
 print(f"Your savings are: ${savings:.2f}")
 print(f"Your total left to spend is: ${total:.2f}")
 
-print(percent("rent", rent))
-print(percent("utilities", utilities))
-print(percent("groceries", groceries))
-print(percent("transportation", transportation))
-print(percent("savings", savings))
-print(percent("expenses", expenses))
-
-income = float(dollars("income",income))
-rent = float(dollars("rent", rent))
-groceries = float(dollars("rent", rent))
-utilities = float(input("How much is utilities? "))
-transportation = float(input("How much is transportation? "))
-rent = float(input("How much is rent? "))
+print(percent("rent",(rent)))
+print(percent("utilities", (utilities)))
+print(percent("groceries", (groceries)))
+print(percent("transportation", (transportation)))
+print(percent("savings", (savings)))
+print(percent("expenses", (expenses)))
