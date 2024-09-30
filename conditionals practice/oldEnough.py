bc304d67309774efc11a8942d24b4967ca1d44b4
MarcusDLC <marcus.delacruz@ucas-edu.net>
2024-09-30 20:40:16 +0000

    Finished the old enough assigment in Python

---
+++ conditionals practice/oldEnough.py
@@ -0,0 +1,18 @@
+
+age = int(input("This program calculates whether you qualify to do these specific actions. Please enter your age to begin."))
+if age >= 18:
+ print("You qualify to vote!")
+else:
+ print("You do not qualify to vote")
+if age >= 16:
+ print("You qualify to drive!")
+else:
+ print("You do not qualify to drive.")
+if age >= 15:
+ print("You qualify to get a learner's permit!")
+else:
+ print("You do not qualify to get a learner's permit")
+if age >= 5:
+ print("You qualify to go to school!")
+else:
+ print("You do not qualify to go to school")
