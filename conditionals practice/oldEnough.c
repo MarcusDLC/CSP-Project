f6965f159a7b768c8f8028d77b4c32c50ca1ed83
MarcusDLC <marcus.delacruz@ucas-edu.net>
2024-10-01 20:33:17 +0000

    Adjusted the old enough assignment to work as expected

--- conditionals practice/oldEnough.c
+++ conditionals practice/oldEnough.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 int main(void){
-    
+    int age;
+    printf("This program calculates whether you qualify to do these specific actions. Please enter your age to being the process");
+    scanf("%d", &age);
+    if (age >= 18){
+        printf("You qualify to vote!\n");
+    }else{
+        printf("You do not qualify to vote.\n");
+    }if (age >= 16){
+        printf("You qualify to drive!\n");
+    }else{
+        printf("You do not qualify to drive.\n");
+    }if (age >= 15){
+        printf("You qualify to get a learner's permit!\n");
+    }else{
+        printf("You do not qualify to get a learner's permit.\n");
+    }if (age >= 5){
+        printf("You qualify to got to school!\n");
+    }else{
+        printf("You do not qualify to go to school.\n");
     return 0;
-}
+}}
