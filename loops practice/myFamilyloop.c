003e1e4a2d9f14488bd533e599198cc733ac61a7
MarcusDLC <marcus.delacruz@ucas-edu.net>
2024-10-09 20:10:56 +0000

    Finished the Family assignment in C

--- loops practice/myFamilyloop.c
+++ loops practice/myFamilyloop.c
@@ -2,6 +2,7 @@
 int main(void){
     char members[10][20] = {"Marcus", "Katherin", "Fairus", "Marcus", "Aytana", "Micheal"};
     int i;
+    printf("My family:\n");
     for(i=0; i<6; i++){
         printf("%s\n Hello ", members[i]);
     }
